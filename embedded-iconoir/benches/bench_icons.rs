8548fe5f0acce84adc4a3eafd1cafa6b82b643f6
Yandrik <me@yandrik.dev>
2023-03-22 03:15:27 +0100

    test: added test and benchmark

---
+++ embedded-iconoir/benches/bench_icons.rs
@@ -0,0 +1,59 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use embedded_graphics::image::Image;
+use embedded_graphics::mock_display::MockDisplay;
+use embedded_graphics_core::pixelcolor::BinaryColor;
+use embedded_graphics_core::prelude::{ImageDrawable, Point};
+use embedded_graphics_core::Drawable;
+use embedded_iconoir::icons;
+use embedded_iconoir::prelude::IconoirNewIcon;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut disp = MockDisplay::new();
+    disp.set_allow_overdraw(true);
+    disp.set_allow_out_of_bounds_drawing(true);
+
+    let mut group = c.benchmark_group("Render Different Resolutions");
+
+    let icon = icons::size144px::actions::AddCircle::new(BinaryColor::On);
+    let icon = Image::new(&icon, Point::zero());
+    group.bench_function("render 144px icon", |b| {
+        b.iter(|| {
+            icon.draw(&mut disp).unwrap();
+        })
+    });
+
+    let icon = icons::size96px::actions::AddCircle::new(BinaryColor::On);
+    let icon = Image::new(&icon, Point::zero());
+    group.bench_function("render 96px icon", |b| {
+        b.iter(|| {
+            icon.draw(&mut disp).unwrap();
+        })
+    });
+
+    let icon = icons::size48px::actions::AddCircle::new(BinaryColor::On);
+    let icon = Image::new(&icon, Point::zero());
+    group.bench_function("render 48px icon", |b| {
+        b.iter(|| {
+            icon.draw(&mut disp).unwrap();
+        })
+    });
+
+    let icon = icons::size24px::actions::AddCircle::new(BinaryColor::On);
+    let icon = Image::new(&icon, Point::zero());
+    group.bench_function("render 24px icon", |b| {
+        b.iter(|| {
+            icon.draw(&mut disp).unwrap();
+        })
+    });
+
+    let icon = icons::size12px::actions::AddCircle::new(BinaryColor::On);
+    let icon = Image::new(&icon, Point::zero());
+    group.bench_function("render 12px icon", |b| {
+        b.iter(|| {
+            icon.draw(&mut disp).unwrap();
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
---
+++ embedded-iconoir/tests/render_test.rs
@@ -0,0 +1,66 @@
+use embedded_graphics::mock_display::MockDisplay;
+use embedded_graphics_core::geometry::OriginDimensions;
+use embedded_graphics_core::image::ImageDrawable;
+use embedded_graphics_core::pixelcolor::BinaryColor;
+use embedded_iconoir::size24px::actions;
+use embedded_iconoir::{icons, Icon};
+use paste::paste;
+
+macro_rules! test_render_icon {
+    ( $module_path:path, $cat:ident::$icon:ident ) => {{
+        test_render_icon!($module_path::$cat::$icon)
+    }};
+    ( $path:path ) => {{
+        let mut disp = MockDisplay::new();
+        let icon: Icon<_, $path> = Icon::new(BinaryColor::On);
+        icon.draw(&mut disp)
+    }};
+}
+
+macro_rules! test_render_icons {
+    ( $name:ident, $res:ident::$module:ident, $($cat:ident::$icon:ident),*$(,)? ) => {
+        #[test]
+        fn $name() -> anyhow::Result<()> {
+            $(
+                println!("testing icon {}...", stringify!($res::$module::$cat::$icon));
+                test_render_icon!($res::$module::$cat::$icon)?;
+            )*
+            Ok(())
+        }
+    }
+}
+
+#[test]
+fn temp() {
+    let disp: MockDisplay<BinaryColor> = MockDisplay::new();
+    println!("dimensions: {}", disp.size());
+}
+
+macro_rules! test_render_icons_premade {
+    ($name:ident, $res:ident::$module:ident) => {
+        test_render_icons!(
+            $name,
+            $res::$module,
+            activities::Archery,
+            emojis::EmojiBlinkRight,
+            cloud::CloudDesync,
+            three_d_editor::ThreedEllipse,
+            actions::Restart,
+            animals::Fish,
+            animations::Keyframes,
+            database::DatabaseSettings,
+            design_tools::BorderTr,
+            devices::SaveFloppyDisk,
+        );
+    };
+}
+
+test_render_icons_premade!(test_12px, icons::size12px);
+test_render_icons_premade!(test_18px, icons::size18px);
+test_render_icons_premade!(test_24px, icons::size24px);
+test_render_icons_premade!(test_32px, icons::size32px);
+test_render_icons_premade!(test_48px, icons::size48px);
+
+// doesn't work as MockDisplay as a hard-coded size of 64px for now
+// test_render_icons_premade!(test_96px, icons::size96px);
+// test_render_icons_premade!(test_144px, icons::size144px);
