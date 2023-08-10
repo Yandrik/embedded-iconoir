9064a0a841238b941eb7e4b0a6ae153d3baed117
Yandrik <me@yandrik.dev>
2023-08-10 12:33:17 +0200

    example(tester): added some 12px icons to see how they render

--- examples/src/bin/tester.rs
+++ examples/src/bin/tester.rs
@@ -3,11 +3,11 @@ use embedded_graphics::image::Image;
 use embedded_graphics::pixelcolor::Rgb888;
 use embedded_graphics::prelude::*;
 
-use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay, Window};
+use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay, Window, SimulatorEvent};
 use embedded_iconoir::prelude::IconoirNewIcon;
 use embedded_iconoir::size18px::{actions, activities, buildings, cloud};
 use embedded_iconoir::size24px;
-use embedded_iconoir::{size18px, size32px};
+use embedded_iconoir::{size12px, size18px, size32px};
 
 type Color = Rgb888;
 
@@ -145,9 +145,80 @@ fn main() {
         30,
     );
 
+    // 12px
+
+    draw_icon(
+        &mut display,
+        &size12px::buildings::Church::new(Rgb888::CSS_CORAL),
+        7,
+        2,
+        12,
+        12,
+    );
+    draw_icon(
+        &mut display,
+        &size12px::actions::Cancel::new(Rgb888::CSS_DARK_SLATE_GRAY),
+        8,
+        2,
+        12,
+        12,
+    );
+    draw_icon(
+        &mut display,
+        &size12px::activities::Archery::new(Rgb888::CSS_ALICE_BLUE),
+        9,
+        2,
+        12,
+        12,
+    );
+    draw_icon(
+        &mut display,
+        &size12px::buildings::Church::new(Rgb888::CSS_CORAL),
+        10,
+        2,
+        12,
+        12,
+    );
+    draw_icon(
+        &mut display,
+        &size12px::buildings::ChurchAlt::new(Rgb888::CSS_CHOCOLATE),
+        8,
+        3,
+        12,
+        12,
+    );
+    draw_icon(
+        &mut display,
+        &size12px::cloud::CloudSync::new(Rgb888::CSS_CYAN),
+        9,
+        3,
+        12,
+        12,
+    );
+    draw_icon(
+        &mut display,
+        &size32px::development::ElectronicsChip::new(Rgb888::CSS_CRIMSON),
+        10,
+        3,
+        12,
+        12,
+    );
+
     let output_settings = OutputSettingsBuilder::new()
         .scale(1)
         // .theme(Theme::OledBlue)
         .build();
-    Window::new("Hello World", &output_settings).show_static(&display);
+
+    let mut window = Window::new("Hello World", &output_settings);
+
+    'outer: loop {
+        window.update(&display);
+        for evt in window.events() {
+            match evt {
+                SimulatorEvent::Quit => break 'outer,
+                _ => {}
+            }
+        }
+    }
+
 }
